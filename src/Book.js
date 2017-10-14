d8b2eb833f0b6f315a61dd998539e2ee6466f172
anjali0626 <a9sharma@scu.edu>
2017-10-14 14:37:31 -0700

    Create Book class

---
+++ src/Book.js
@@ -0,0 +1,14 @@
+class Book {
+
+  constructor() {
+    // TODO...
+    // properties of Book go here
+  }
+
+  // Add methods of Book class like this:
+  methodName() {
+    // TODO...
+    // function definition goes here
+  }
+
+};
