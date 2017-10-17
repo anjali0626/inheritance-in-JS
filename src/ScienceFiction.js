8445e29ba1af6dffef71a04852b08f72ad6af359
anjali0626 <a9sharma@scu.edu>
2017-10-17 11:42:26 -0700

    Create ScienceFiction class

---
+++ src/ScienceFiction.js
@@ -0,0 +1,15 @@
+class ScienceFiction extends Fiction {
+
+  constructor() {
+    super();
+    // TODO...
+    // properties of ScienceFiction go here
+  }
+
+  // Add methods of ScienceFiction class like this:
+  methodName() {
+    // TODO...
+    // function definition goes here
+  }
+
+};
