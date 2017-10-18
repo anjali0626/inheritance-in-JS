9eece2990f1469bd41db83ec6f5e2b55160ec05b
anjali0626 <a9sharma@scu.edu>
2017-10-18 16:32:20 -0700

    Create Autobiography class

---
+++ src/Autobiography.js
@@ -0,0 +1,15 @@
+class Autobiography extends NonFiction {
+
+  constructor() {
+    super();
+    // TODO...
+    // properties of Autobiography go here
+  }
+
+  // Add methods of Autobiography class like this:
+  methodName() {
+    // TODO...
+    // function definition goes here
+  }
+
+};
