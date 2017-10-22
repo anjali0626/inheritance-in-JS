fdb46445ff705bf22387fa648b1f46313a21d500
anjali0626 <a9sharma@scu.edu>
2017-10-21 18:38:02 -0700

    Add properties of Autobiography class

---
+++ src/Autobiography.js
@@ -0,0 +1,14 @@
+class Autobiography extends NonFiction {
+
+  constructor() {
+    super();
+    this.genre = 'Non Fiction: Autobiography';
+  }
+
+  // Add methods of Autobiography class like this:
+  methodName() {
+    // TODO...
+    // function definition goes here
+  }
+
+};
